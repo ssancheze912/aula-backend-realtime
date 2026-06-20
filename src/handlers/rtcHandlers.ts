0b3cb2e90c2cf16031c28d88b8ad9159ea4c8cb4
crlscastillo161@gmail.com <crlscastillo161@gmail.com>
2026-06-20 01:46:26 -0500

    add back logs

--- src/handlers/rtcHandlers.ts
+++ src/handlers/rtcHandlers.ts
@@ -5,24 +5,32 @@ export function registerRtcHandlers(io: Server, socket: Socket): void {
   // `rtc:peer_ready` de los que ya estaban (eso solo lo reciben los presentes), así que
   // aprende quién es cada cual a partir de la oferta que le llega (US-12 / malla P2P).
   socket.on('rtc:offer', ({ to, offer, userId, username, avatarUrl }) => {
+    console.log('[RTC-BE] offer recibido', { from: socket.id, to })
     io.to(to).emit('rtc:offer', { from: socket.id, offer, userId, username, avatarUrl })
+    console.log('[RTC-BE] offer reenviado', { from: socket.id, to })
   })
 
   socket.on('rtc:answer', ({ to, answer, userId, username, avatarUrl }) => {
+    console.log('[RTC-BE] answer recibido', { from: socket.id, to })
     io.to(to).emit('rtc:answer', { from: socket.id, answer, userId, username, avatarUrl })
+    console.log('[RTC-BE] answer reenviado', { from: socket.id, to })
   })
 
   socket.on('rtc:ice', ({ to, candidate }) => {
+    console.log('[RTC-BE] ICE recibido', { from: socket.id, to })
     io.to(to).emit('rtc:ice', { from: socket.id, candidate })
+    console.log('[RTC-BE] ICE reenviado', { from: socket.id, to })
   })
 
   socket.on('rtc:ready', ({ roomId, userId, username, avatarUrl }) => {
+    console.log('[RTC-BE] ready recibido', { socketId: socket.id, roomId })
     socket.to(roomId).emit('rtc:peer_ready', {
       socketId: socket.id,
       userId,
       username,
       avatarUrl,
     })
+    console.log('[RTC-BE] ready reenviado', { socketId: socket.id, roomId })
   })
 
   socket.on('rtc:media_state', ({ roomId, userId, isMuted, isCameraOff, isScreenSharing }) => {
